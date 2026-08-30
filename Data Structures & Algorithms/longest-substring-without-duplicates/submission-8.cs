f3d03b3019a1e31f2720353a9af5f96122a1e1ef
Hermes <hermeschigoziri@gmail.com>
2026-08-30 23:02:02 +0100

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cs
@@ -0,0 +1,33 @@
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+{
+    if (s.Length <= 1) return s.Length;
+    var table = new Dictionary<char, int>();
+    int l = 0, r = 0;
+    int length = 0;
+    while (r < s.Length)
+    {
+        if (!table.ContainsKey(s[r]))
+        {
+            table[s[r]] = r;
+        }
+        else
+        {
+            length = Math.Max(length, r - l);
+            // Remove all key-value pair outside our window
+            while (table.ContainsKey(s[r])) {
+                table.Remove(s[l]);
+                l += 1;
+            }
+            table[s[r]] = r;
+        }
+        r += 1;
+    }
+
+    length = Math.Max(length, r - l);
+
+
+    return length;
+}
+}
